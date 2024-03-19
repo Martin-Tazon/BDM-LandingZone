e453e7e2e3a0ea7cfe66f466339274fc627e491f
Hopercsd <alextrem@csd.auth.gr>
2024-03-19 19:45:35 +0100

    make ploader for csvs universal. Run it from cmd giving the path of csv files as a system argument

--- ploader_opendatabcn.py
+++ ploader_opendatabcn.py
@@ -4,6 +4,7 @@ import json
 import pymongo
 import datetime
 import pandas as pd
+import sys
 
 # Set up the connection
 client = pymongo.MongoClient("mongodb+srv://alex_martin:1234@lab1.37jadij.mongodb.net/")
@@ -20,16 +21,25 @@ def csv_to_json(csv_file):
     return lines 
 
 
-opendatabcn_path = "../data/opendatabcn-income/"
-files = sorted(glob(opendatabcn_path + "*.csv"))
+# opendatabcn_path = "../data/opendatabcn-income/"
+# files = sorted(glob(opendatabcn_path + "*.csv"))
 
-for file in files:
-    fn = file.split("/")[-1].split(".")[0]
-    print(fn)
+def add_file_to_db(path):
+    files = sorted(glob(path + "*.csv"))
+    print(path, files)
+    # for file in files:
+    #     fn = file.split("/")[-1].split(".")[0]
+    #     print(fn)
 
-    # Create a collection for the CSV file
-    current_time = datetime.datetime.now()
-    collection = db[fn + "-" + current_time.strftime("%Y-%m-%dT%H:%M:%S")]
+    #     # Create a collection for the CSV file
+    #     current_time = datetime.datetime.now()
+    #     collection = db[fn + "-" + current_time.strftime("%Y-%m-%dT%H:%M:%S")]
 
-    # Insert the JSON data into the collection
-    collection.insert_many(csv_to_json(file))
+    #     # Insert the JSON data into the collection
+    #     collection.insert_many(csv_to_json(file))
+
+
+if __name__ == "__main__":
+    path= sys.argv[1]
+    print(path)
+    add_file_to_db(path)
