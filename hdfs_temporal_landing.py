51ccc9063f672d4c80994c01e6ec8d04e4e578a1
Martin Tazon <martin.tazon.tdr@gmail.com>
2024-04-05 18:02:15 +0200

    Persintent landing script

--- hdfs_temporal_landing.py
+++ hdfs_temporal_landing.py
@@ -1,83 +1,122 @@
-import pandas as pd
 from hdfs import InsecureClient
 import os
-from glob import glob
 import sys
 
+sources = ['idealista', 'lookup_tables', 'opendatabcn-income']
+path_temporal_hdfs='user/bdm/temporal_landing/'
 
-def add_csv_to_hdfs(path):
-    files = sorted(glob(path + "*.csv"))
-    print(files)
-    for file in files:
-        fn = file.split("\\")[-1]
-        path_v = path.split("/")[1]
-        dest_path = f'user/bdm/temporal_landing/{path_v}/{fn}'
-        
-        # Check if the file already exists in HDFS, if so, skip
-        if client_hdfs.status(dest_path, strict=False) is not None:
-            print(f"File {fn} already exists in HDFS. Skipping...")
-            continue
+def ls_hdfs(client, path: str) -> list[str]:
+    """
+    List the files and directories in the specified HDFS path.
 
-        df = pd.read_csv(file)
-        with client_hdfs.write(f'user/bdm/temporal_landing/{path_v}/{fn}', encoding = 'utf-8') as writer:
-            df.to_csv(writer)
+    Args:
+        client (InsecureClient): The HDFS client object.
+        path (str): The HDFS path to list.
+
+    Returns:
+        list[str]: A list of file and directory names in the specified path.
+    """
+    if not path.startswith('user/bdm/'):
+        path = 'user/bdm/' + path
+    return client.list(path)
+
+def du_hdfs(client, path: str) -> None:
+    """
+    Display the size of the specified HDFS path and its subdirectories.
+
+    Args:
+        client (InsecureClient): The HDFS client object.
+        path (str): The HDFS path to check.
+
+    Returns:
+        None
+    """
+    if not path.startswith('user/bdm/'):
+        path = 'user/bdm/' + path
+    content = client.content(path)
+    print(f"Size of {path}: {content['length']} bytes")
+    if content['directoryCount'] > 0:
+        print(f"Path contains {content['directoryCount']} subdirectories.")
+        subdirs = ls_hdfs(client,path)
+        for sdir in subdirs:
+            content_sd = client.content(path+sdir)
+            print(f"    {sdir}: {content_sd['length']} bytes")
+
+def rm_hdfs(client, path: str, recursive: bool = True):
+    """
+    Delete the specified HDFS path.
+
+    Args:
+        client (InsecureClient): The HDFS client object.
+        path (str): The HDFS path to delete.
+        recursive (bool, optional): Whether to delete subdirectories recursively. Defaults to True.
+
+    Returns:
+        None
+    """
+    return client.delete(path, recursive=recursive)
+
+def upload_source_to_hdfs(client, source: str) -> None:
+    """
+    Upload the files from the local directory to the specified HDFS path.
+
+    Args:
+        client (InsecureClient): The HDFS client object.
+        source (str): The name of the source directory.
+
+    Returns:
+        None
+    """
+    local_path = f"data/{source}/"
+    hdfs_path = path_temporal_hdfs + source
+
+    client.makedirs(hdfs_path)
+    for f in os.listdir(local_path):
+        try:
+            client.upload(hdfs_path, local_path + f)
+        except:
+            print(f"These {f} already exist")
+
+def download_source_to_hdfs(client, source: str, outdir: str) -> None:
+    """
+    Download the files from the specified HDFS path to the local directory.
+
+    Args:
+        client (InsecureClient): The HDFS client object.
+        source (str): The name of the source directory.
+
+    Returns:
+        None
+    """
+    outdir = outdir + "/" + source + "/"
+    hdfs_path = path_temporal_hdfs + source
+
+    for f in client.list(hdfs_path):
+        client.download(hdfs_path + f, outdir + f)
 
 
-def add_json_to_hdfs(path):
-    files = sorted(glob(path + "*.json"))
-    print(files)
-    for file in files:
-        fn = os.path.basename(file)  # Get the filename without the directory path
-        path_v = os.path.basename(os.path.dirname(path))  # Get the second-level directory name
-        dest_path = f'user/bdm/temporal_landing/{path_v}/{fn}'
-        
-        # Check if the file already exists in HDFS, if so, skip
-        if client_hdfs.status(dest_path, strict=False) is not None:
-            print(f"File {fn} already exists in HDFS. Skipping...")
-            continue
-        
-        with open(file, 'r', encoding='utf-8') as f:
-            content = f.read()  # Read the content of the JSON file
-        with client_hdfs.write(dest_path, encoding='utf-8') as writer:
-            writer.write(content)  # Write the content to HDFS
-
-# with client_hdfs.read('user/bdm/temporal_landing/idealista/2020_12_31_idealista.json', encoding = 'utf-8') as reader:
-#   df = pd.read_csv(reader,index_col=0)
-#   print(df)
-
-
-def empty_directory_hdfs(directory):
-    # Recursively delete all files and subdirectories within the directory
-    client_hdfs.delete(directory, recursive=True)
-
-# Specify the directory you want to empty
-directory_to_empty = 'user/bdm/temporal_landing/idealista'
-
-# Empty the directory in HDFS
-# empty_directory_hdfs(directory_to_empty)
 
 
 if __name__ == "__main__":
 
+    # To connect to WebHDFS by providing the IP of the HDFS host and the WebHDFS port.
     client_hdfs = InsecureClient('http://10.4.41.44:9870/', user='bdm')
+    print(client_hdfs)
 
-    sources = ['idealista', 'lookup_tables', 'opendatabcn-income']
+    removeFlag = int(sys.argv[1])
+    if removeFlag:
+        # Initialize temporal landing dir
+        print("Initializing landing zone ...")
+        rm_hdfs(client_hdfs, path_temporal_hdfs)
+        client_hdfs.makedirs(path_temporal_hdfs)
 
+    # Upload sources
+    
     for source in sources:
-        client_hdfs.makedirs(f'user/bdm/temporal_landing/{source}')
-
-    # Access the database
-    path = sys.argv[1]
-    format = sys.argv[2]
-
-    if format=='csv':
-        add_csv_to_hdfs(path)
-    elif format=='json':
-        add_json_to_hdfs(path)
-    else:
-        print('not supported file. give json or csv')
+        print("Uploading",source,"...")
+        upload_source_to_hdfs(client_hdfs, source)
+    print("Upload finished!")
 
 
-# RUN EXAMPLES:
-# python hdfs_temporal_landing.py "data/opendatabcn-income/" "csv"
-# python hdfs_temporal_landing.py "data/idealista/" "json" 
+    print("Landing zone status:")
+    du_hdfs(client_hdfs,path_temporal_hdfs)
